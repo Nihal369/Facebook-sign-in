f4d34e0651a590e4e3047d573774fbe4f3ac886a
Nihal Ismail <nihalismailk@gmail.com>
2016-07-16 15:40:12 +0530

    First working model :dancer:

--- app/src/main/java/com/lmntrx/android/nihal/facebooksign/MainActivity.java
+++ app/src/main/java/com/lmntrx/android/nihal/facebooksign/MainActivity.java
@@ -3,32 +3,57 @@ package com.lmntrx.android.nihal.facebooksign;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
-        callbackManager=CallbackManager.Factory.create();
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_main);
-        LoginButton loginButton=(LoginButton)findViewById(R.id.loginButton);
+        LoginButton loginButton = (LoginButton) findViewById(R.id.loginButton);
         loginButton.setReadPermissions("email");
+        //loginButton.setReadPermissions("user_friends");
+        //loginButton.setReadPermissions("public_profile");
+       // loginButton.setReadPermissions("user_birthday");
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                Profile myProfile = Profile.getCurrentProfile();
+
+                Log.i("Mava", myProfile.getName());
+                Log.i("Mava", myProfile.getFirstName());
+                Log.i("Mava", myProfile.getId());
+                Log.i("Mava", myProfile.getLastName());
+
+                ProfilePictureView picture=(ProfilePictureView)findViewById(R.id.profilePicView);
+                picture.setProfileId(Profile.getCurrentProfile().getId());
+                TextView text=(TextView)findViewById(R.id.textView);
+                text.setText(myProfile.getName());
             }
 
             @Override
